94cee9007e0d6efc8295b981eb5f82582af0845d
Neo Babic <neo.babic@elev.ga.ntig.se>
2023-09-11 13:17:03 +0200

    Network jeff

---
+++ Assets/Scripts/BulletController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletController : MonoBehaviour
+{
+
+  [SerializeField]
+  float initialVelocity = 115f;
+
+  [SerializeField]
+  float lifeTime = 8.5f;
+
+  void Start()
+  {
+    Rigidbody rigidbody = GetComponent<Rigidbody>();
+    rigidbody.velocity = transform.forward * initialVelocity;
+
+    Destroy(this.gameObject, lifeTime);
+  }
+}
